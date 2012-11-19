feb0274c2e0c4238a8b9e9106213f2ef4c886de3
Patrick <Patrickeps@gmx.de>
2012-11-19 19:43:26 +0100

    browser resize support
    - for black hightlighting blocks
    - for clickable zones

--- jquery.onscreen-help.js
+++ jquery.onscreen-help.js
@@ -61,7 +61,7 @@
 			}
 			
 			//calc coords 2
-			var x2 = x1 + width +4; // + 2 because of border
+			var x2 = x1 + width + 4; // + 2 because of border
 			var y2 = y1 + height;
 			
 			// calc size info objects
@@ -113,14 +113,10 @@
 			self.$b3.update(calc.b3);
 			self.$b4.update(calc.b4);
 			
-			$('body,html').animate({
-				scrollTop : (calc.b1.height - 50)
-			}, 1000);
-			
 		};
 		
 		/* Creates or updates the jQuery element for the description box */
-		function _initDescriptionBox(position){
+		function _initDescriptionBox(position) {
 			if (!self.$descriptionOuter) {
 				//create description speech bubble if not yet existing
 				self.$descriptionBubble = $("<div class='osh_arrow_box' />");
@@ -129,7 +125,7 @@
 				$("body").append(self.$descriptionOuter);
 			}
 			
-			if(!position){
+			if (!position) {
 				position = "bottom";
 			}
 			// remove old position class and add new
@@ -159,32 +155,48 @@
 			
 		};
 		
+		/* Repositions the clickable zones when user resizes the browser window */
+		self.reposClickableZones = function (steps) {
+			
+			$.each(steps, function (index, step) {
+				_positionClickableZone(step.$zone, step);
+			});
+		};
+		
+		/* Calculates the position and sets its css properties for a clickable zone*/
+		function _positionClickableZone($zone, step) {
+			
+			var $elem = step.$elem;
+			var offset = $elem.offset();
+			$zone.css({
+				"top" : offset.top + paddingTop($elem),
+				"left" : offset.left + paddingLeft($elem),
+				"width" : $elem.width(),
+				"height" : $elem.height()
+			});
+			
+			// add child class if this step has a parent step
+			if (step.parent) {
+				
+				if (!$zone.hasClass("osh_is_child")) {
+					$zone.addClass("osh_is_child");
+				}
+				$zone.css({
+					"left" : $zone.offset().left
+				});
+				$zone.width($zone.width());
+			}
+			
+		}
+		
 		/* Creates clickable buttons over the targeted step elements */
 		self.buildClickableZones = function (steps, stepActivationCallback) {
 			
 			$.each(steps, function (index, step) {
-				console.debug("zone creating");
 				
-				var $elem = $(step.selector);
-				var offset = $(step.selector).offset();
-				
-				var $zone = $("<div class='osh_marked_zone' />")
-					.appendTo("body")
-					.css({
-						"top" : offset.top + paddingTop($elem),
-						"left" : offset.left + paddingLeft($elem),
-						"width" : $elem.width(),
-						"height" : $elem.height()
-					});
-					
-				// add child class if this step has a parent step
-				
-				if(step.parent){
-					
-					$zone.addClass("osh_is_child");
-					$zone.css({"left":$zone.offset().left});
-					$zone.width($zone.width());
-				}
+				//create zone and position it
+				var $zone = $("<div class='osh_marked_zone' />").appendTo("body");
+				_positionClickableZone($zone, step);
 				
 				$("<div/>").append($("<p />").text(step.title)).appendTo($zone);
 				
@@ -192,7 +204,7 @@
 				step.$zone = $zone;
 				
 				//bind step activation callback to $zone click event
-				step.$zone.click(function(e){
+				step.$zone.click(function (e) {
 					stepActivationCallback.call(self, step);
 				});
 				
@@ -288,11 +300,11 @@
 	var TutorialController = function () {
 		var self = this;
 		
-		/* Searches up the DOM for a tutorial step which would be a parent to the 
-		*  given jQuery element and returns the step
-		*/
+		/* Searches up the DOM for a tutorial step which would be a parent to the
+		 *  given jQuery element and returns the step
+		 */
 		function tryGetParent(steps, $elem) {
-		
+			
 			var resultStep;
 			$.each(steps, function (j, parStep) {
 				if ($elem.parents(parStep.selector).length > 0) {
@@ -355,32 +367,29 @@
 			self.highlightCallback.call(self, newStep.selector, newStep.addPadding);
 			self.showDescriptionCallback.call(self, newStep);
 			
-			
-			
-			
-			
 			//remove the active css class from the previous link
 			if (_currStep) {
 				_currStep.$link.removeClass("active");
 				_currStep.$zone.show();
 				
-				if(_currStep.parent){
+				if (_currStep.parent) {
 					_currStep.parent.$zone.show();
 				}
 			}
 			
-			
-			if (newStep.parent){
+			if (newStep.parent) {
 				newStep.parent.$zone.hide();
 			}
-			
-			
 			
 			//add active css class to new $link
 			newStep.$zone.hide();
 			newStep.$link.addClass("active");
 			_currStep = newStep;
 			
+			// scroll to steps target
+			$('body,html').animate({
+				scrollTop : (newStep.$elem.offset().top + paddingTop(newStep.$elem) - 50)
+			}, 1000);
 			
 		};
 		
@@ -407,6 +416,14 @@
 				
 				// activate step
 				self.activateStep(_stepsIndexed[nextIndex]);
+			}
+		};
+		
+		/* Reacts on browser resize and updates the UI elements */
+		self.browserResize = function () {
+			
+			if (_currStep) {
+				self.highlightCallback.call(self, _currStep.selector, _currStep.addPadding);
 			}
 		};
 	};
@@ -439,7 +456,7 @@
 		// You already have access to the DOM element and the options via the instance,
 		// e.g., this.element and this.options
 		
-		
+		var self = this;
 		// jQuery elem toolbar navigation buttons
 		this.$buttons = undefined;
 		
@@ -460,6 +477,12 @@
 		this.toolbarCreator = new ToolbarCreator();
 		this.toolbarCreator.createToolbarBasic(this.tutorialController.nextPrevStep);
 		this.toolbarCreator.createToolbarButtons(indexedSteps, this.tutorialController.activateStep);
+		
+		// react on window resize event
+		$(window).resize(function () {
+			self.tutorialController.browserResize();
+			self.highlighter.reposClickableZones(indexedSteps);
+		});
 		
 	};
 	
